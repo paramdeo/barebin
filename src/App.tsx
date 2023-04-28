cc200200c1ed94da0ff2aeb51336773bff51c613
Paramdeo Singh <paramdeo@paramdeo.com>
2023-04-27 22:44:09 -0400

    Fix textarea wrapping, increase maxlength to 1000, implement copy to clipboard for linked bins

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,21 @@ const fromBase64 = (str: string) => {
 
 function App() {
   const [state, setState] = useState('')
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const messageRef: string | any = useRef('')
 
   const queryParameters = new URLSearchParams(window.location.search)
-  const existingURL = queryParameters.get("b")
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const existingURL: string | any = queryParameters.get("b")
 
   const getURL = () => {
     navigator.clipboard.writeText(window.location + '?b=' + toBase64(messageRef.current.value))
+  }
+
+  const toClipboard = () => {
+    navigator.clipboard.writeText(fromBase64(existingURL))
   }
 
   const newBin = () => {
@@ -37,17 +44,15 @@ function App() {
     <h1>BareBin</h1>
     <p>A free, simple, and stateless pastebin.</p>
       <textarea
-        id="message"
-        name="message"
         rows={15}
         cols={40}
-        maxLength={500}
+        maxLength={1000}
         ref={messageRef}
         defaultValue={state}
         readOnly={ existingURL ? true : false} />
       <br/>
       <button onClick={() => getURL()} className={ existingURL ? 'hidden' : 'button' }>Get Shareable URL</button>
-      <button onClick={() => newBin()} className={ existingURL ? 'button' : 'hidden' }>Copy to Clipboard</button>
+      <button onClick={() => toClipboard()} className={ existingURL ? 'button' : 'hidden' }>Copy to Clipboard</button>
       <button onClick={() => newBin()} className={ existingURL ? 'button' : 'hidden' }>New BareBin Snippet</button>
       <p className="footer">Copyright &copy; <a href="https://paramdeo.com" target="_blank" rel="noopener" title="Personal website of Paramdeo Singh">Paramdeo K. Singh</a> &middot; Made with ☕️ in 🇬🇾</p>
     </>
