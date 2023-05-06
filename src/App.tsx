dce818c58f25e18ef3e031fa1822fa860c1dce86
Paramdeo Singh <paramdeo@paramdeo.com>
2023-05-05 23:38:50 -0400

    Replaced useEffect, useState, and useRef with Recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useRef, useEffect } from 'react'
+import { RecoilRoot, atom, useRecoilState } from 'recoil';
+
 import './App.css'
 
 const toBase64 = (str: string) => {
@@ -12,47 +13,67 @@ const fromBase64 = (str: string) => {
   return decodeURIComponent(atob(str))
 }
 
-function App() {
+// Grab URL query parameters
+const queryParameters = new URLSearchParams(window.location.search)
+
+// Grab 'b?=' query parameter
+const existingURL = queryParameters.get("b")
+
+const messageState = atom({
+  key: 'messageState',
+  default: ''
+})
+
+function Message() {
   // Track state of message content
-  const [message, setMessage] = useState('')
-
-  // Capture snapshot of message when copying to clipboard
-  const messageRef: string | any = useRef('')
-
-  // Grab URL query parameters
-  const queryParameters = new URLSearchParams(window.location.search)
-
-  // Grab 'b?=' query parameter
-  const existingURL: string | any = queryParameters.get("b")
+  const [message, setMessage] = useRecoilState(messageState)
 
   const createShareableURL = () => {
     // Concatenate base URL, query parameter, and Base64 encoded string
-    navigator.clipboard.writeText(window.location + '?b=' + toBase64(messageRef.current.value))
+    navigator.clipboard.writeText(window.location + '?b=' + toBase64(message))
   }
 
   const copyToClipboard = () => {
     // Copy decoded message to the clipboard
-    navigator.clipboard.writeText(fromBase64(existingURL))
+    navigator.clipboard.writeText(message)
   }
 
   const newMessage = () => {
-    // Reload and clear message state and URL parameter
+    // Clear message state and URL parameter
     setMessage('')
     window.location.replace(window.location.origin)
   }
 
-  const updateMessageLength = (event: any) => {
-    // Update character length counter
+  const onChange = (event: any) => {
     setMessage(event.target.value)
   }
 
-  useEffect(() => {
-    // Run once on page load if URL parameter is present
-    if (existingURL) {
-      setMessage(fromBase64(existingURL))
-    }
-  }, [existingURL]) // URL parameter dependency
+  // If URL parameter is present, decode and set the message
+  if (existingURL) {
+    setMessage(fromBase64(existingURL))
+  }
 
+  return (
+    <>
+      <textarea
+        rows={12}
+        cols={40}
+        maxLength={1000}
+        onChange={onChange}
+        readOnly={ existingURL ? true : false}
+        value={message}
+      />
+      <p className="flex-end">{message.length} / 1000</p>
+      <button onClick={createShareableURL} className={ existingURL ? 'hidden' : 'button' }>Get Shareable URL</button>
+      <p className="flex-center">
+        <button onClick={copyToClipboard} className={ existingURL ? 'button' : 'hidden' }>Copy Content to Clipboard</button>&emsp;
+        <button onClick={newMessage} className={ existingURL ? 'button' : 'hidden' }>New BareBin Snippet</button>
+      </p>
+    </>
+  )
+}
+
+function Header() {
   return (
     <>
       <br/>
@@ -60,21 +81,26 @@ function App() {
       <h1>BareBin</h1>
       <p>A free, simple, and stateless pastebin</p>
       </div>
-      <textarea
-        name="message"
-        rows={12}
-        cols={40}
-        maxLength={1000}
-        ref={messageRef}
-        defaultValue={message}
-        onChange={updateMessageLength}
-        readOnly={ existingURL ? true : false}
-      />
-      <p className="flex-end">{message.length} / 1000</p>
-      <button onClick={createShareableURL} className={ existingURL ? 'hidden' : 'button' }>Get Shareable URL</button>
-      <button onClick={copyToClipboard} className={ existingURL ? 'button' : 'hidden' }>Copy Content to Clipboard</button>
-      <button onClick={newMessage} className={ existingURL ? 'button' : 'hidden' }>New BareBin Snippet</button>
-      <p className="footer">Copyright &copy; <a href="https://paramdeo.com" target="_blank" rel="noopener" title="Personal website of Paramdeo Singh">Paramdeo Singh</a> &middot; Made with ☕️ in 🇬🇾<br/><a href="https://github.com/paramdeo/barebin" target="_blank" rel="noopener" title="@paramdeo/barebin">Source on GitHub</a></p>
+    </>
+  )
+}
+
+function Footer() {
+  return (
+    <>
+      <p className="footer">Copyright &copy; <a href="https://paramdeo.com" target="_blank" rel="noopener" title="Personal website of Paramdeo Singh">Paramdeo Singh</a> &middot; Made with ☕️ in 🇬🇾<br/><a href="https://github.com/paramdeo/barebin" target="_blank" rel="noopener" title="@paramdeo/barebin">github/paramdeo/barebin</a></p>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <RecoilRoot>
+      <Message/>
+    </RecoilRoot>
+    <Footer/>
     </>
   )
 }
